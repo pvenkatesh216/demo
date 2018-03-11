95eca21cfdfb250b170571133819f22b56b4f1b9
pvenkatesh216 <pvenkatesh216@gmail.com>
2018-03-11 12:54:16 +0530

    first commit frmo local

---
+++ NG/src/test/NewTest2.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Welcome 2");
+	  welcome.First();
+	  System.out.println("New message after first commits");
+	  System.out.println("Commit from Git");
+		  
+
+  }
+}
